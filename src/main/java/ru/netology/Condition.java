ae65990a70b7556b85299adeb0a61a67c23a82c4
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-04-14 11:53:13 +0300

    First commit

---
+++ src/main/java/ru/netology/Condition.java
@@ -0,0 +1,83 @@
+package ru.netology;
+
+public class Condition {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        if (currentTemperature > maxTemperature) {
+            return;
+        }
+        if (currentTemperature < minTemperature) {
+            return;
+        }
+        // здесь уверены, что все проверки прошли
+        this.currentTemperature = currentTemperature;
+    }
+
+//    public void setCurrentTemperature(int currentTemperature) {
+//        if (currentTemperature <= maxTemperature) {
+//            if (currentTemperature >= minTemperature) {
+//                this.currentTemperature = currentTemperature;
+//            }
+//        }
+//    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void increaseCurrentTemperature() {
+        if (!this.isOn())
+            return;
+        if (this.getCurrentTemperature() + 1 >= this.maxTemperature)
+            return;
+        this.setCurrentTemperature(this.currentTemperature + 1);
+
+    }
+
+    public void decreaseCurrentTemperature() {
+        if (!this.isOn())
+            return;
+        if (this.getCurrentTemperature() - 1 <= this.minTemperature)
+            return;
+        this.setCurrentTemperature(this.currentTemperature - 1);
+    }
+}
+
+
---
+++ src/test/java/ru/netology/ConditionTest.java
@@ -0,0 +1,104 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionTest {
+
+
+    @Test
+    public void shouldGetAndSetName() {
+
+        Condition condition = new Condition();
+        String expected = "Кондишн";
+        assertNull(condition.getName());
+        condition.setName(expected);
+        assertEquals(expected, condition.getName());
+    }
+
+    @Test
+    public void shouldGetAndSetOn() {
+        Condition condition = new Condition();
+        boolean expected = true;
+        condition.setOn(expected);
+        assertEquals(expected, condition.isOn());
+    }
+
+    @Test
+    public void shouldGetAndSetCurrentT() {
+        Condition condition = new Condition();
+        condition.setMaxTemperature(40);
+        condition.setMinTemperature(10);
+        int expected = 20;
+        condition.setCurrentTemperature(expected);
+        assertEquals(expected, condition.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldGetAndSetMinTemperature() {
+        Condition condition = new Condition();
+        int expected = 10;
+        condition.setMinTemperature(expected);
+        assertEquals(expected, condition.getMinTemperature());
+    }
+
+    @Test
+    public void shouldGetAndSetMaxTemperature() {
+        Condition condition = new Condition();
+        int expected = 30;
+        condition.setMaxTemperature(expected);
+        assertEquals(expected, condition.getMaxTemperature());
+    }
+
+    @Test
+    public void shouldIncreaseCurrentTemperature() {
+        Condition condition = new Condition();
+        condition.setOn(true);
+        condition.setMaxTemperature(40);
+        condition.setMinTemperature(10);
+        condition.setCurrentTemperature(20);
+        int expected = 21;
+        condition.increaseCurrentTemperature();
+        assertEquals(expected, condition.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldDecreaseCurrentTemperature() {
+        Condition condition = new Condition();
+        condition.setOn(true);
+
+        condition.setMaxTemperature(40);
+        condition.setMinTemperature(10);
+        condition.setCurrentTemperature(20);
+        int expected = 19;
+        condition.decreaseCurrentTemperature();
+        assertEquals(expected, condition.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldCheckMaxTemperature() {
+        Condition condition = new Condition();
+        condition.setOn(true);
+        condition.setMaxTemperature(20);
+        condition.setCurrentTemperature(20);
+
+        int expected = 20;
+        condition.increaseCurrentTemperature();;
+        assertEquals(expected, condition.getCurrentTemperature());
+    }
+
+    @Test
+    public void shouldCheckMinTemperature() {
+        Condition condition = new Condition();
+        condition.setOn(true);
+        //установить max темпертуру
+        condition.setMinTemperature(10);
+        condition.setMaxTemperature(30);
+        condition.setCurrentTemperature(10);
+
+        int expected = 10;
+        condition.decreaseCurrentTemperature();
+        assertEquals(expected, condition.getCurrentTemperature());
+    }
+}
